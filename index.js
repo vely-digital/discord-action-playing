018233ee89913ce05152eb1c30c7b932f1e8a866
Filip Patafta <fpatafta@live.com>
2020-10-26 21:49:55 +0100

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.set("view engine", "ejs");
 
